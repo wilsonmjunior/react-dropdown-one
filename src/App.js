1e6449aa61f742ae3b4d8e695481f63b27648c1e
Wilson Junior <wilsonjunior@Wilsons-MacBook-Pro.local>
2020-11-26 13:32:49 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.scss';
+import Dropdown from './Dropdown';
+
+const items = [
+  {
+    id: 1,
+    value: 'Pulp Fiction',
+  },
+  {
+    id: 2,
+    value: 'The Prestige',
+  },
+  {
+    id: 3,
+    value: 'Blade Runner 2049',
+  },
+];
+
+function App() {
+  return (
+    <div className="container">
+      <h1 style={{ textAlign: 'center' }}>
+        Buy Movies{' '}
+        <span role="img" aria-label="Movie projector">
+          🎥
+        </span>
+      </h1>
+      <Dropdown title="Select movie" items={items} />
+    </div>
+  );
+}
+
+export default App;
